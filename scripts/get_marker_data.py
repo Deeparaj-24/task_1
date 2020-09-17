f8cdd7e8a165e96f049cf2dbbdffc4313c1e3675
Deeparaj-24 <deeparaj.2409@gmail.com>
2020-09-17 08:53:35 +0530

    Modified launchfiles - remapped respective topics

--- scripts/get_marker_data.py
+++ scripts/get_marker_data.py
@@ -6,26 +6,14 @@ import tf
 
 from geometry_msgs.msg import PoseArray
 
-#Defining a class
-class Marker_detect():
+#Define a class
+class Whycon_detect():
 
 	def __init__(self):
-		rospy.init_node('marker_detection',anonymous=False) # initializing a ros node with name marker_detection
-
-		self.whycon_marker = {}	# Declaring dictionaries
-	
-		rospy.Subscriber('/whycon/poses',PoseArray,self.whycon_data)	# Subscribing to topic
-
-	# Callback for /whycon/poses
-	# Please fill in the function
-	def whycon_data(self,msg):
-
-
-		# Printing the detected markers on terminal
-		print(self.whycon_marker)
+		
 
 
 if __name__=="__main__":
-	marker = Marker_detect()
+	marker = Whycon_detect()
 	while not rospy.is_shutdown():
 		rospy.spin()
