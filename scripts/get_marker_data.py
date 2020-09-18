ee2aa0c87a797e5d5544b9b43f34ce584325bdd6
Deeparaj-24 <deeparaj.2409@gmail.com>
2020-09-18 10:33:11 +0530

    task1.2 commit

--- scripts/get_marker_data.py
+++ scripts/get_marker_data.py
@@ -5,12 +5,22 @@ import roslib
 import tf
 
 from geometry_msgs.msg import PoseArray
+#from geometry_msgs.msg import Pose
 
 #Define a class
 class Whycon_detect():
 
 	def __init__(self):
-		
+		#self.pose = Pose()
+		rospy.init_node('whycon_detection',anonymous=True) # Initiating a node with name whycon detection
+		self.whycon_coordinates = {} #Declaring a ditionary named whycon_coordinates
+		rospy.Subscriber('/whycon/poses',PoseArray,self.whycon_callback)
+	def whycon_callback(self,data):
+		self.position_coordinates = []
+		for i in range(len(data.poses)):
+    			self.position_coordinates.append([data.poses[i].position.x, data.poses[i].position.y, data.poses[i].position.z])
+        		self.whycon_coordinates[int(i)] = self.position_coordinates[i]
+		print self.whycon_coordinates
 
 
 if __name__=="__main__":
